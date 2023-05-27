4b812767cd6da9126f34f70d3ec592e58cf3323a
zmzade <zmahmoodzadeh@yahoo.com>
2023-05-27 21:30:13 +0200

    work with nested routing and routing hooks

---
+++ src/routing/UserDetail.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const UserDetail = () => {
+  const params = useParams();
+  return <p>User {params.id}</p>;
+};
+
+export default UserDetail;
---
+++ src/routing/routes.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+import UserList from "./UserList";
+import ContactPage from "./ContactPage";
+import UserDetail from "./UserDetail";
+import Layout from "./Layout";
+import UsersPage from "./UsersPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <HomePage /> },
+      {
+        path: "users",
+        element: <UsersPage />,
+        children: [{ path: ":id", element: <UserDetail /> }],
+      },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
+]);
+
+export default router;
